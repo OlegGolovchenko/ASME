65bf869694befe52bc017b999cc195623c460ea1
Oleg Golovchenko <IgRoK_be@hotmail.com>
2025-04-02 21:34:31 +0200

    adding needed code and javadoc adding steps in build files to handle javadoc

--- ASME.Plugin/src/org/igrok_net/asme/plugins/interfaces/IStorageProvider.java
+++ ASME.Plugin/src/org/igrok_net/asme/plugins/interfaces/IStorageProvider.java
@@ -24,6 +24,15 @@
  */
 package org.igrok_net.asme.plugins.interfaces;
 
+/**
+ * Storage provider interface.
+ * To create storage provider plugin please
+ * use {@link org.igrok_net.asme.plugins.BaseStorageProvider
+ * BaseStorageProvider}.
+ * 
+ * @author Oleg Golovchenko (<a href="https://igrok-net.org">IgRok-NET</a>)
+ * @version 0.0.1-alpha
+ */
 public interface IStorageProvider {
-
+    String getInitialConfigStorage();
 }
---
+++ ASME.Plugin/src/org/igrok_net/asme/plugins/interfaces/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Plugin interfaces to be used by third party plugins
+ * 
+ * @author Oleg Golovchenko (<a href="https://igrok-net.org">IgRok-NET</a>)
+ * @version 0.0.1-alpha
+ */
+package org.igrok_net.asme.plugins.interfaces;
